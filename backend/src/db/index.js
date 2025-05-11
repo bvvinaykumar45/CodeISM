08c62742cde924dbf2a68233283be4a262d33ad7
bvvinaykumar45 <bvvinaykumar45@gmail.com>
2025-05-11 16:26:19 +0530

    Postgres DB and Prisma configured.

---
+++ backend/src/db/index.js
@@ -0,0 +1,8 @@
+import {PrismaCllient} from "../generated/prisma/index.js";
+
+const globalForPrisma = globalThis;
+
+export const db = globalForPrisma.prisma || new PrismaCllient();
+
+if(process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+
