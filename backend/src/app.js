159b2fe5d6f4549c8a5f8b7aa84c0e78cca3d218
bvvinaykumar45 <bvvinaykumar45@gmail.com>
2025-05-11 20:40:57 +0530

    removing generated/prisma folder from git.

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,4 +7,15 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  return res.status(200).json({
+    success: true,
+    message: "You have successfully pinged Code ISM 🔥."
+  });
+});
+
+app.use('/api/v1/auth');
+
+app.use(express.json());
+
 export default app;
